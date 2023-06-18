4b2ba6f4104ed4803c0ae69aa938821a4dd774aa
Gavin Scheafer <gscheafe@uci.edu>
2023-06-17 20:46:30 -0700

    dialogue now pauses

--- Dialogue/Scripts/tag_dialogue.gd
+++ Dialogue/Scripts/tag_dialogue.gd
@@ -3,6 +3,7 @@ extends DeltascriptTagDialogueBase
 var dialogue_prefab = preload("res://UI/DialogueBox.tscn")
 
 func _line_start():
+	
 	var current_dialogue = dialogue_prefab.instantiate();
 	current_dialogue.word_text = line_text
 	current_dialogue.name_label = event_player.get_event_metadata("Speaker")
--- UI/DialogueBox.gd
+++ UI/DialogueBox.gd
@@ -8,6 +8,7 @@ var name_label : String
 var word_text : String
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	get_tree().paused = true
 	$Panel/VBoxContainer/Name.text = name_label
 	$Panel/VBoxContainer/Dialogue.text = word_text
 	pass # Replace with function body.
@@ -16,6 +17,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("jump"):
+		get_tree().paused = false
 		dialogue_closed.emit()
 		queue_free()
 	pass
